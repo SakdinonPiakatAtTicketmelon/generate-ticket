90fd410cb95e847441f10bac761e2dd177d61882
Sakdinon Piakat <sakdinon.p@ticketmelon.com>
2020-05-12 15:42:55 +0700

    up

--- index.js
+++ index.js
@@ -1,102 +1,148 @@
-import express from 'express'
-import csv from 'csvtojson'
+import express from "express";
+import csv from "csvtojson";
 
-import path from 'path'
-import formidable from 'formidable'
-import fs from 'fs'
-import bodyParser from 'body-parser'
-import util from 'util'
+import path from "path";
+import formidable from "formidable";
+import fs from "fs";
+import bodyParser from "body-parser";
+import util from "util";
 
+const app = express();
 
-const app = express()
+app.set("views", __dirname + "/views");
+app.use("/asset", express.static(__dirname + "/asset"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "ejs");
 
-app.set('views', __dirname + '/views');
-app.use("/asset", express.static(__dirname + '/asset'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs');
+app.get("/", (req, res) => {
+  res.render(path.join(__dirname + "/views/index.html"));
+});
 
-app.get('/', (req, res) => {
-  res.render(path.join(__dirname + '/views/index.html'))
-})
-
-app.post('/template', (req, res) => {
-  let data = []
+app.post("/template", (req, res) => {
+  let data = [];
   var form = new formidable.IncomingForm();
   form.parse(req, (err, fields, files) => {
     var oldpath = files.filetoupload.path;
-    var newpath = './tmp' + files.filetoupload.name;
-    fs.rename(oldpath, newpath, (err) => {
+    var newpath = "./tmp" + files.filetoupload.name;
+    fs.rename(oldpath, newpath, err => {
       if (err) throw err;
       csv()
         .fromFile(newpath)
-        .on('json', (jsonObj, rowIndex) => {
-          data.push(jsonObj)
+        .on("json", (jsonObj, rowIndex) => {
+          data.push(jsonObj);
         })
-        .on('done', (error) => {
-          fs.unlink(newpath, (err) => {
+        .on("done", error => {
+          fs.unlink(newpath, err => {
             if (err) throw err;
           });
-          let template = fields.template
+          let template = fields.template;
           switch (template) {
-            case '16':
-              res.render(path.join(__dirname + '/views/Ticket_template_default.html'), { data: data })
+            case "test":
+              res.render(path.join(__dirname + "/views/testPdf.html"), {
+                data: data
+              });
               break;
-            case '15':
-              res.render(path.join(__dirname + '/views/joox.html'), { data: data })
+            case "16":
+              res.render(
+                path.join(__dirname + "/views/Ticket_template_default.html"),
+                { data: data }
+              );
               break;
-            case '1':
-              res.render(path.join(__dirname + '/views/ticket1.html'), { data: data })
+            case "15":
+              res.render(path.join(__dirname + "/views/joox.html"), {
+                data: data
+              });
               break;
-            case '2':
-              res.render(path.join(__dirname + '/views/ticket2.html'), { data: data })
+            case "1":
+              res.render(path.join(__dirname + "/views/ticket1.html"), {
+                data: data
+              });
               break;
-            case '2.1':
-              res.render(path.join(__dirname + '/views/ticket2.1.html'), { data: data })
+            case "2":
+              res.render(path.join(__dirname + "/views/ticket2.html"), {
+                data: data
+              });
               break;
-            case '3':
-              res.render(path.join(__dirname + '/views/ticket3.html'), { data: data })
+            case "2.1":
+              res.render(path.join(__dirname + "/views/ticket2.1.html"), {
+                data: data
+              });
               break;
-            case '4':
-              res.render(path.join(__dirname + '/views/ticket4.html'), { data: data })
+            case "3":
+              res.render(path.join(__dirname + "/views/ticket3.html"), {
+                data: data
+              });
               break;
-            case '5':
-              res.render(path.join(__dirname + '/views/ticket5.html'), { data: data })
+            case "4":
+              res.render(path.join(__dirname + "/views/ticket4.html"), {
+                data: data
+              });
               break;
-            case '6':
-              res.render(path.join(__dirname + '/views/ticket_Epic_Poll_Party.html'), { data: data })
+            case "5":
+              res.render(path.join(__dirname + "/views/ticket5.html"), {
+                data: data
+              });
               break;
-            case '7':
-              res.render(path.join(__dirname + '/views/ticket_maya_complimentary.html'), { data: data })
+            case "6":
+              res.render(
+                path.join(__dirname + "/views/ticket_Epic_Poll_Party.html"),
+                { data: data }
+              );
               break;
-            case '8':
-              res.render(path.join(__dirname + '/views/ticket_maya_general.html'), { data: data })
+            case "7":
+              res.render(
+                path.join(__dirname + "/views/ticket_maya_complimentary.html"),
+                { data: data }
+              );
               break;
-            case '9':
-              res.render(path.join(__dirname + '/views/ticket_maya_QR_Only.html'), { data: data })
+            case "8":
+              res.render(
+                path.join(__dirname + "/views/ticket_maya_general.html"),
+                { data: data }
+              );
               break;
-            case '10':
-              res.render(path.join(__dirname + '/views/e-ticket-03.html'), { data: data })
+            case "9":
+              res.render(
+                path.join(__dirname + "/views/ticket_maya_QR_Only.html"),
+                { data: data }
+              );
               break;
-            case '11':
-              res.render(path.join(__dirname + '/views/maya-a4.html'), { data: data })
+            case "10":
+              res.render(path.join(__dirname + "/views/e-ticket-03.html"), {
+                data: data
+              });
               break;
-            case '12':
-              res.render(path.join(__dirname + '/views/TRANSMISSION.html'), { data: data })
+            case "11":
+              res.render(path.join(__dirname + "/views/maya-a4.html"), {
+                data: data
+              });
               break;
-            case '13':
-              res.render(path.join(__dirname + '/views/QR_only_digi_top.html'), { data: data })
+            case "12":
+              res.render(path.join(__dirname + "/views/TRANSMISSION.html"), {
+                data: data
+              });
               break;
-            case '14':
-              res.render(path.join(__dirname + '/views/KnockKnock2019.html'), { data: data })
+            case "13":
+              res.render(
+                path.join(__dirname + "/views/QR_only_digi_top.html"),
+                { data: data }
+              );
+              break;
+            case "14":
+              res.render(path.join(__dirname + "/views/KnockKnock2019.html"), {
+                data: data
+              });
               break;
             default:
-              res.render(path.join(__dirname + '/views/ticket1.html'), { data: data })
+              res.render(path.join(__dirname + "/views/ticket1.html"), {
+                data: data
+              });
           }
-        })
+        });
     });
   });
-})
+});
 
-app.listen(5000, function () {
-  console.log('Example app listening on port 5000!')
-})
+app.listen(5000, function() {
+  console.log("Example app listening on port 5000!");
+});
